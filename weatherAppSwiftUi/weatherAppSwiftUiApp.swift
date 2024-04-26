aca8fe9d673093dbcb31735fc60c0b1b23b8a673
theomarie <theomarie@protonmail.com>
2024-04-26 17:38:26 +0200

    init project

---
+++ weatherAppSwiftUi/weatherAppSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppSwiftUiApp.swift
+//  weatherAppSwiftUi
+//
+//  Created by Theo Marie on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
